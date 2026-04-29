5f22b332a40fc91316d57c00cb781e3e95629046
Alberto190973 <65360142+Alberto190973@users.noreply.github.com>
2026-04-28 19:25:53 -0600

    Add files via upload

---
+++ GLOBALS.py
@@ -0,0 +1,29 @@
+LANGS = ["en", "es", "fr", "de", "it", "pt", "ru", "zh", "ja", "ko"]
+
+LANGS_NAMES = {
+    "en": "English",
+    "es": "Spanish",
+    "fr": "French",
+    "de": "German",
+    "it": "Italian",
+    "pt": "Portuguese",
+    "ru": "Russian",
+    "zh": "Chinese",
+    "ja": "Japanese",
+    "ko": "Korean",
+}
+
+COLORSFLAGS = {
+    "en": "#1f77b4",
+    "es": "#ff7f0e",
+    "fr": "#2ca02c",
+    "de": "#d62728",
+    "it": "#9467bd",
+    "pt": "#8c564b",
+    "ru": "#e377c2",
+    "zh": "#7f7f7f",
+    "ja": "#bcbd22",
+    "ko": "#17becf"
+}
+
+CACA = "caca"
